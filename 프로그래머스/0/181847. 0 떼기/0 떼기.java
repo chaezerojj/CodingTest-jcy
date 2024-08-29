6fd49dc2575c46c283f34c88932570305e8e646e
chaezerojj <yyzz1219@gmail.com>
2024-08-29 19:16:59 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int z = Integer.parseInt(n_str);
+        answer = Integer.toString(z);
+        return answer;
+    }
+}
