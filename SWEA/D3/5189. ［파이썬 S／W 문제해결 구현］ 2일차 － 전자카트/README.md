c703586153d4f1f922c4247b4deab27ce08052d4
chaezerojj <yyzz1219@gmail.com>
2025-09-08 17:45:18 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 2일차 - 전자카트, Time: 113 ms, Memory: 61,568 KB -BaekjoonHub

---
+++ SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/README.md
@@ -0,0 +1,15 @@
+# [D3] [파이썬 S/W 문제해결 구현] 2일차 - 전자카트 - 5189 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWTtmmdKeD8DFAVT) 
+
+### 성능 요약
+
+메모리: 61,568 KB, 시간: 113 ms, 코드길이: 731 Bytes
+
+### 제출 일자
+
+2025-09-08 17:45
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
@@ -0,0 +1,29 @@
+def dfs(lev, sum_v):
+    global min_v
+
+    if sum_v >= min_v: return
+
+    if lev == N - 1:
+        # 마지막 구역에서 사무실로 돌아오는 비용
+        sum_v += arr[path[-1]][0]
+        min_v = min(min_v, sum_v)  # 최소값 갱신
+        return
+
+    for i in range(1, N):  # branch: N - 1
+        if used[i] == 1: continue
+        used[i] = 1
+        path.append(i)
+        dfs(lev + 1, sum_v + arr[path[-2]][i])
+        path.pop()
+        used[i] = 0
+
+t = int(input())
+for tc in range(1, t + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    used = [0] * N
+    path = [0]  # 사무실 0으로 초기화
+    used[0] = 1  # 사무실 방문처리
+    min_v = float('inf')
+    dfs(0, 0)
+    print(f"#{tc} {min_v}")
