337ef446d37db134a792dd0bcb76aa65b1a4ff80
chaezerojj <yyzz1219@gmail.com>
2025-09-14 19:13:20 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 81 ms, Memory: 60,672 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,20 +1,11 @@
 for tc in range(1, 11):
     N = int(input())
-    height = list(map(int, input().split()))
+    heights = list(map(int, input().split()))
     view = 0
 
-    # 앞 0 0 뒤 0 0 제외하고 범위 설정
-    for b in range(2, N - 2):
-        # 조망권 범위: 양 옆 각 2칸씩 확보되어야 함
-        # - 앞 2칸, 뒤 3칸(중간 1칸포함)
-        check_view = height[b - 2:b + 3]
-        # 원본 복사
-        # 원본으로는 가운데 건물 확인. 복사본으로는 가장 높은 건물, 두번째로 높은 건물 확인
-        check_view_cp = check_view[:]
-        # 복사본 내림차순 정렬
-        check_view_cp.sort(reverse=True)
-        # 현재 건물이 구간에서 가장 높은 건물인지 확인
-        if check_view[2] == check_view_cp[0]:
-            # 현재 건물 높이 - 주변에서 가장 높은 건물 높이 = 조망권 확보 층수
-            view += check_view_cp[0] - check_view_cp[1]
+    for i in range(2, N - 2):
+        max_height = max(heights[i - 2], heights[i - 1], heights[i + 1], heights[i + 2])
+        if heights[i] > max_height:
+            # 조망권 세대 수 = 현재 건물 높이 - 최대 건물 높이
+            view += heights[i] - max_height
     print(f"#{tc} {view}")
